0b00816ca6d117e02788b9ab96071f65a6bbab01
Daan <daanj33@gmail.com>
2021-09-30 21:19:10 +0200

    Add checkbox

---
+++ src/stories/checkbox/Checkbox.stories.js
@@ -0,0 +1,44 @@
+import TwCheckbox from './Checkbox.vue';
+
+export default {
+  title: 'Tailwind/Checkbox',
+  component: TwCheckbox,
+  argTypes: { // Additional information that can be provided for each prop. It will automatically detect type & default state from Vue SFC, if provided
+    color: {
+      name: 'color',
+      defaultValue: 'indigo',
+      description: 'Tailwind classname for outline and fill color',
+      control: { type: 'text' }
+    },
+    size: {
+      name: 'size',
+      description: 'Set size of checkbox',
+      control: { type: 'number', min: 3 }
+    },
+    value: {
+      name: 'value',
+      description: 'Value of checkbox'
+    },
+    label: {
+      name: 'label',
+      description: 'Toggle text-only button'
+    }
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { TwCheckbox },
+  template: 
+    `
+    <tw-checkbox v-bind="$props" />
+    `,
+});
+
+export const Standard = Template.bind({});
+Standard.args = {
+  color: 'indigo',
+  size: 6,
+  value: 'some_value',
+  label: 'Checkbox',
+};
---
+++ src/stories/checkbox/Checkbox.vue
@@ -0,0 +1,62 @@
+<template>
+	<div :class="`flex items-center text-${color}-800 text-sm capitalize`">
+		<div class="flex items-center">
+			<input 
+				:id="id" 
+				:value="value"
+				@input="update($event)"
+				type="checkbox" 
+				:class="`opacity-0 absolute h-${size} w-${size} cursor-pointer z-20`" 
+			/>
+			<div :class="`bg-white border-2 rounded relative border-${color}-700 w-${size} h-${size} flex flex-shrink-0 justify-center items-center`">
+				<div :class="`hidden bg-${color}-700 w-${size - 2} h-${size - 2} absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] rounded-sm pointer-events-none`" />
+			</div>
+		</div>
+		<div class="ml-3 text-sm">
+			<label 
+				:for="id" 
+				class="cursor-pointer font-medium text-gray-700"
+			>{{ label }}</label>
+		</div>
+	</div>
+</template>
+
+<script>
+export default { 
+	name: 'Checkbox',
+	props: {
+		value: {
+			type: [Boolean, String],
+			default: null
+		},
+		label: {
+			type: String,
+			default: null
+		},
+		color: {
+			type: String,
+			default: 'indigo'
+		},
+		size: {
+			type: Number,
+			default: 5
+		}
+	},
+	methods: {
+		update(e) {
+				this.$emit('update:value', !this.value);
+		}
+	},
+	computed: {
+		id() {
+			return (Math.random() + 1).toString(36).substring(7);
+		}
+	}
+};
+</script> 
+
+<style scoped>
+input[type="checkbox"]:checked + div div {
+  display: block;
+}
+</style>
