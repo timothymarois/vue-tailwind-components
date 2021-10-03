3b10c7f16b1edc2958376b85400b266610eeacc5
Timothy Marois <timothymarois@gmail.com>
2021-10-03 09:26:41 -0400

    Updated import paths

--- src/components/Elements/Base/index.js
+++ src/components/Elements/Base/index.js
@@ -1,7 +1,6 @@
 import BaseButton from '@/components/Elements/Base/BaseButton'
 import BaseInput from '@/components/Elements/Base/BaseInput'
 import BaseLabel from '@/components/Elements/Base/BaseLabel'
-import BaseLoader from '@/components/Elements/Base/BaseLoader'
 import BaseIcon from '@/components/Elements/Base/BaseIcon'
 import BaseOverlay from '@/components/Elements/Base/BaseOverlay'
 import BaseCheckbox from '@/components/Elements/Base/BaseCheckbox'
@@ -10,7 +9,6 @@ export {
     BaseButton,
     BaseInput,
     BaseLabel,
-    BaseLoader,
     BaseIcon,
     BaseOverlay,
     BaseCheckbox
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,16 @@
 import { BaseInput, BaseLabel, BaseButton, BaseIcon, BaseOverlay, BaseCheckbox } from "@/components/Elements/Base";
 
-import TButton from '@/components/Elements/TButton'
 import TLoader from '@/components/Elements/TLoader'
+import TButton from '@/components/Elements/TButton'
 
+import TTextField from '@/components/Forms/TTextField'
+import TSelect from '@/components/Forms/TSelect'
+import TCheckbox from '@/components/Forms/TCheckbox'
+
+
+// these need updating
+import TableSimple from '@/components/Tables/TableSimple'
+import Pagination from '@/components/Tables/Pagination'
 import Icon from '@/components/Elements/Icon'
 import Modal from '@/components/Elements/Modal'
 import Card from '@/components/Elements/Card'
@@ -11,23 +19,28 @@ import Tabs from '@/components/Elements/Tabs'
 import Badge from '@/components/Elements/Badge'
 
 export {
-    
-    TButton,
     TLoader,
+    TButton,
 
+    TTextField,
+    TSelect,
+    TCheckbox,
+    
     BaseButton,
     BaseInput,
     BaseLabel,
     BaseIcon,
     BaseOverlay,
     BaseCheckbox,
-
+    
     Icon,
     Modal,
     Card,
     Dropdown,
     Tabs,
-    Badge
+    Badge,
+    TableSimple,
+    Pagination
 }
 
 export default { }
