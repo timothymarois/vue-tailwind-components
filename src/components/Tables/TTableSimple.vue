8acd5943e89fb3d216a4188755f169a77f116292
Timothy Marois <timothymarois@gmail.com>
2021-10-06 17:04:01 -0400

    Added cursor pointer on selected rows

--- src/components/Tables/TTableSimple.vue
+++ src/components/Tables/TTableSimple.vue
@@ -28,7 +28,12 @@
         <tbody v-if="items && items.length > 0" class="tbody bg-white overflow-scroll">
             <tr 
                 v-for="(item, i) in items" :key="i" 
-                :class="[`hover:bg-${selectedColor}-100 transition duration-150 text-gray-800 hover:text-indigo-900`, (item.class ? item.class+' trow' : 'trow'), (selection.includes(i) ? `bg-${selectedColor}-100` : '')]"
+                :class="[
+                    `hover:bg-${selectedColor}-100 transition duration-150 text-gray-800 hover:text-indigo-900`, 
+                    (item.class ? item.class+' trow' : 'trow'), 
+                    (selection.includes(i) ? `bg-${selectedColor}-100` : ''),
+                    (selectFromRow) ? 'cursor-pointer' : ''
+                ]"
                 @click="selectRow(i)"
             >
                 <td 
