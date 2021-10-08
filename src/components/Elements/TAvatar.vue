4358474678d08428f2e46f99521205f86bb35998
Daan <daanj33@gmail.com>
2021-10-08 19:06:59 +0200

    Add TAvatar component

---
+++ src/components/Elements/TAvatar.vue
@@ -0,0 +1,33 @@
+<template>
+	<div :class="`h-${size} w-${size} bg-${color} text-${textColor} font-semibold text-lg flex justify-center items-center ${round ? 'rounded-full' : ''}`">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'TAvatar',
+	props: {
+		size: {
+			type: [String, Number],
+			required: false,
+			default: 6
+		},
+		color: {
+			type: String,
+			required: false,
+			default: 'indigo-700'
+		},
+		textColor: {
+			type: String,
+			required: false,
+			default: 'white'
+		},
+		round: {
+			type: Boolean,
+			required: false,
+			default: true
+		}
+	}
+};
+</script>
