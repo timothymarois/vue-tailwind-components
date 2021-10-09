520129362b6af6bd2ef973a78b4fed4f0551c544
Timothy Marois <timothymarois@gmail.com>
2021-10-09 14:33:24 -0400

    Added new icons

--- src/components/Elements/Base/BaseIcon.vue
+++ src/components/Elements/Base/BaseIcon.vue
@@ -85,6 +85,10 @@
 
     <path v-else-if="value=='menu'" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
 
+    <path v-else-if="value=='pencil-alt'" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+
+    <path v-else-if="value=='trash'" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+
 
   </svg>
 </template>
