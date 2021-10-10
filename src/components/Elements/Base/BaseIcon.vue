32c47efb05bc097937fb0b6eebc51130fdafdf5f
Timothy Marois <timothymarois@gmail.com>
2021-10-09 20:19:47 -0400

    Fixing icon

--- src/components/Elements/Base/BaseIcon.vue
+++ src/components/Elements/Base/BaseIcon.vue
@@ -91,7 +91,7 @@
 
     <path v-else-if="value=='download'" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
 
-    <path v-else-if="value=='document-search'" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+    <path v-else-if="value=='document-search'" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 21h7a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v11m0 5l4.879-4.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242z" />
 
   </svg>
 </template>
