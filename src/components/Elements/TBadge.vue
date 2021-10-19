87e666394826a4a85cb8cad79fcb27d3dfc73f53
Daan <daanj33@gmail.com>
2021-10-19 19:10:04 +0200

    Add rounded option to badge component

--- src/components/Elements/TBadge.vue
+++ src/components/Elements/TBadge.vue
@@ -28,33 +28,45 @@ export default {
     props: {
         color: {
             type: String,
+            required: false,
             default: 'indigo'
         },
-        removeable : {
+        removeable: {
             type: Boolean,
+            required: false,
             default: false
         },
         outlined: {
             type: Boolean,
+            required: false,
             default: false
         },
+        rounded: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
     },
     computed: {
         contentClasses() {
-            let c = ['inline-flex rounded-full items-center py-0.5 text-sm font-medium'];
+            let c = ['inline-flex items-center py-0.5 text-sm font-medium'];
 
             if (this.removeable) {
                 c = c.concat(['pl-2','pr-0.5']);
-            }
-            else {
+            } else {
                 c = c.concat(['px-2']);
             }
 
             if (this.outlined) {
-                c = c.concat(['border-'+this.color+'-800','bg-'+this.color+'-100','text-'+this.color+'-800']);
+                c = c.concat([`border-${this.color}-800','bg-${this.color}-100','text-${this.color}-800`]);
+            } else {
+                c = c.concat([`border-${this.color}-100','bg-${this.color}-100','text-${this.color}-800`]);
             }
-            else {
-                c = c.concat(['border-'+this.color+'-100','bg-'+this.color+'-100','text-'+this.color+'-800']);
+
+            if (this.rounded) {
+                c = c.concat(['rounded-full']);
+            } else {
+                c = c.concat(['rounded-sm'])
             }
 
             return c;
