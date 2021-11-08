da24b105473a5d37d19b7b77294c1e2615952308
Daan <daanj33@gmail.com>
2021-11-08 21:34:07 +0100

    Add vertical stepper option

--- src/components/Elements/TStepper.vue
+++ src/components/Elements/TStepper.vue
@@ -1,30 +1,64 @@
 <template>
 	<div>
-		<div class="flex flex-row justify-between">
-			<template v-for="(step, i) of stepOptions">
-				<hr v-if="i != 0" class="flex-grow h-0.5 ml-4 mr-4 mt-5 bg-gray-300" :key="i" />
-				<div class="relative" :key="step.id">
-					<div 
-						class="flex items-center justify-center rounded-full h-12 w-12 text-lg cursor-pointer select-none"
-						:class="currentStep >= i + 1 ? `bg-${color} text-${textColor}` : 'bg-gray-400 text-white'"
-					>
-						<t-icon v-if="currentStep > i + 1" value="check" size="6" />
-						<span v-else>
-							{{ +i + 1 }}
-						</span>
+		<div v-if="horizontal">
+			<div class="flex flex-row justify-between">
+				<template v-for="(step, i) of stepOptions">
+					<hr v-if="i != 0" class="flex-grow h-0.5 ml-4 mr-4 mt-5 bg-gray-300" :key="i" />
+					<div class="relative" :key="step.id">
+						<div 
+							class="flex items-center justify-center rounded-full h-12 w-12 text-lg cursor-pointer select-none"
+							:class="currentStep >= i + 1 ? `bg-${color} text-${textColor}` : 'bg-gray-400 text-white'"
+						>
+							<t-icon v-if="currentStep > i + 1" value="check" size="6" />
+							<span v-else>
+								{{ +i + 1 }}
+							</span>
+						</div>
+						<div 
+							class="absolute text-center mt-2 w-40 left-1/2 transform -translate-x-1/2"
+							:class="{'text-gray-400' : currentStep < i + 1}"
+						>{{ step.title }}</div>
+					</div>
+				</template>
+			</div>
+			<div class="mt-20" v-for="(step, i) of stepOptions" :key="step.id">
+				<div v-show="(currentStep - 1) == i">
+					<slot :name="step.id" />
+				</div>
+			</div>
+		</div>
+		<div v-else>
+			<div class="flex flex-col justify-between">
+				<div v-for="(step, i) of stepOptions" :key="step.id">
+					<div class="relative">
+						<div 
+							class="flex items-center justify-center rounded-full h-8 w-8 text-lg cursor-pointer select-none"
+							:class="currentStep >= i + 1 ? `bg-${color} text-${textColor}` : 'bg-gray-400 text-white'"
+						>
+							<t-icon v-if="currentStep > i + 1" value="check" size="6" />
+							<span v-else>
+								{{ +i + 1 }}
+							</span>
+						</div>
+						<div 
+							class="absolute left-10 top-0.5 text-lg"
+							:class="{'text-gray-400' : currentStep < i + 1}"
+						>{{ step.title }}</div>
 					</div>
 					<div 
-						class="absolute text-center mt-2 w-40 left-1/2 transform -translate-x-1/2"
-						:class="{'text-gray-400' : currentStep < i + 1}"
-					>{{ step.title }}</div>
+						class="ml-4 my-2 px-8 pt-2 pb-6"
+						:class="{'border-l-2 border-gray-300': i + 1 !== stepOptions.length}"
+						style="min-height: 2rem;"
+					>
+						<transition name="expand" @enter="enter" @after-enter="afterEnter" @leave="leave">
+							<div v-show="(currentStep - 1) == i">
+								<slot :name="step.id" id="stepper-slot" />
+							</div>
+						</transition>
+					</div>
 				</div>
-			</template>
+			</div>
 		</div>
-		<div class="mt-20" v-for="(step, i) of stepOptions" :key="step.id">
-            <div v-show="(currentStep - 1) == i">
-                <slot :name="step.id" />
-            </div>
-        </div>
 	</div>
 </template>
 
@@ -37,6 +71,11 @@ export default {
 		TIcon
 	},
 	props: {
+		horizontal: {
+			type: Boolean,
+			required: false,
+			default: false
+		},
 		color: {
 			type: String,
 			required: false,
@@ -56,6 +95,61 @@ export default {
 			required: true,
 			default: 1
 		}
+	},
+	methods: {
+		enter(element) {
+			const width = getComputedStyle(element).width;
+
+			element.style.width = width;
+			element.style.position = 'absolute';
+			element.style.visibility = 'hidden';
+			element.style.height = 'auto';
+
+			const height = getComputedStyle(element).height;
+
+			element.style.width = null;
+			element.style.position = null;
+			element.style.visibility = null;
+			element.style.height = 0;
+
+			getComputedStyle(element).height;
+
+			requestAnimationFrame(() => {
+				element.style.height = height;
+			});
+		},
+		afterEnter(element) {
+			element.style.height = 'auto';
+		},
+		leave(element) {
+			const height = getComputedStyle(element).height;
+			
+			element.style.height = height;
+
+			getComputedStyle(element).height;
+
+			requestAnimationFrame(() => {
+				element.style.height = 0;
+			});
+		},
 	}
 }
 </script>
+
+<style scoped>
+.expand-enter-active, .expand-leave-active {
+	transition: height .3s cubic-bezier(.25,.8,.5,1);
+	overflow: hidden;
+}
+
+.expand-enter, .expand-leave-to {
+	height: 0;
+}
+
+#stepper-slot {
+	will-change: height;
+	transform: translateZ(0);
+	backface-visibility: hidden;
+	perspective: 1000px;
+}
+</style>
--- stories/TStepper.stories.js
+++ stories/TStepper.stories.js
@@ -18,7 +18,7 @@ export default {
 	}
 };
 
-export const Standard = (args, { argTypes }) => ({
+export const Horizontal = (args, { argTypes }) => ({
 	props: Object.keys(argTypes),
 	components: { TStepper, TButton },
 	data: () => ({
@@ -46,7 +46,57 @@ export const Standard = (args, { argTypes }) => ({
 		`
 		<div>
 			<div class="w-2/3 mx-auto">
-				<t-stepper :stepOptions="stepOptions1" :currentStep="currentStep1" v-bind="$props">
+				<t-stepper :stepOptions="stepOptions1" :currentStep="currentStep1" :horizontal="true" v-bind="$props">
+					<template v-slot:step_1>
+						Content for step 1
+						<t-button @click="currentStep1 += 1">Next step</t-button>
+					</template>
+					<template v-slot:step_2>
+						Content for step 2
+						<t-button @click="currentStep1 += 1">Next step</t-button>
+					</template>
+					<template v-slot:step_3>
+						Content for step 3
+						<t-button @click="currentStep1 += 1">Next step</t-button>
+					</template>
+					<template v-slot:step_4>
+						Content for step 4
+					</template>
+				</t-stepper>
+			</div>
+		</div>
+		`,
+});
+
+export const Vertical = (args, { argTypes }) => ({
+	props: Object.keys(argTypes),
+	components: { TStepper, TButton },
+	data: () => ({
+		stepOptions1: [
+			{
+				title: "Sign up",
+				id: "step_1"
+			},
+			{
+				title: "Confirm email",
+				id: "step_2"
+			},
+			{
+				title: "Select preferences",
+				id: "step_3"
+			},
+			{
+				title: "Finalize sign up",
+				id: "step_4"
+			}
+		],
+		currentStep1: 1
+	}),
+	template: 
+		`
+		<div>
+			<div class="w-2/3 mx-auto">
+				<t-stepper :stepOptions="stepOptions1" :currentStep="currentStep1" :horizontal="false" v-bind="$props">
 					<template v-slot:step_1>
 						Content for step 1
 						<t-button @click="currentStep1 += 1">Next step</t-button>
