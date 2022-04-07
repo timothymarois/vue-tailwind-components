b84e8946c3f161205ba769a5b2a57aee1c201774
Daan <47603391+daan33@users.noreply.github.com>
2022-04-07 16:05:28 +0200

    Revert "Change stop icon to be outlined"
    This reverts commit d373253bdebe55d93ca5b5b22db7a92634747855.

--- src/components/Elements/Base/BaseIconOutline.vue
+++ src/components/Elements/Base/BaseIconOutline.vue
@@ -177,11 +177,6 @@
 
   		<path v-else-if="value=='pause'" stroke-linecap="round" stroke-linejoin="round" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z" />
 
-		<template v-else-if="value=='stop'">
-			<path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-			<path stroke-linecap="round" stroke-linejoin="round" d="M9 10a1 1 0 011-1h4a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 01-1-1v-4z" />
-		</template>
-
 		<path v-else-if="value=='heart'" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
 
 		<path v-else-if="value=='lock-closed'" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
