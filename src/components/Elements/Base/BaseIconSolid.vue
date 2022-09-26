23462c232f824b657ea1d9b5baf72516b87077aa
Aito Hideki <aito.hideki@gmail.com>
2022-09-26 14:39:36 -0400

    updated solid save icon

--- src/components/Elements/Base/BaseIconSolid.vue
+++ src/components/Elements/Base/BaseIconSolid.vue
@@ -54,7 +54,7 @@
 
         <path v-else-if="value=='stop'" fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8 7a1 1 0 00-1 1v4a1 1 0 001 1h4a1 1 0 001-1V8a1 1 0 00-1-1H8z" clip-rule="evenodd" />
 
-		<path v-else-if="value=='save'" d="M15,9H5V5H15M12,19A3,3 0 0,1 9,16A3,3 0 0,1 12,13A3,3 0 0,1 15,16A3,3 0 0,1 12,19M17,3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V7L17,3Z" />
+		<path v-else-if="value=='save'" d="M17 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.89 21 5 21H19C20.1 21 21 20.1 21 19V7L17 3M19 19H5V5H16.17L19 7.83V19M12 12C10.34 12 9 13.34 9 15S10.34 18 12 18 15 16.66 15 15 13.66 12 12 12M6 6H15V10H6V6Z" />
 
         <template v-else-if="value=='chat-alt-2'">
             <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z" />
